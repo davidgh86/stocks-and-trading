a353d51d8858adaac271196c616531db5ef784f7
david <jgarcia.ext@aenor.com>
2019-12-31 14:08:01 +0100

    refactor google charts

--- src/app/stock-chart/stock-chart.component.ts
+++ src/app/stock-chart/stock-chart.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { StockDataProviderService } from '../core/stock-data-provider.service';
-import { AlphaAvantageMapperService } from '../core/alpha-avantage-mapper.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stock-chart',
@@ -10,44 +7,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class StockChartComponent implements OnInit {
 
-  title = '';
-  type = 'CandlestickChart';
-  data = [];
-  options = {
-    legend: 'none',
-    candlestick: {
-      fallingColor: { strokeWidth: 2, stroke: '#a52714' }, // red
-      risingColor: { strokeWidth: 2, stroke: '#0f9d58' }   // green
-    }
-  };
-  width;
-  height;
-
-  constructor(
-    private stockDataProviderService: StockDataProviderService,
-    private alphaAvantageMapperService: AlphaAvantageMapperService,
-    private route: ActivatedRoute
-    ) { }
+  constructor() { }
 
   async ngOnInit() {
-    this.setSize();
-    const symbol = this.route.snapshot.paramMap.get('symbol');
-    const dailyRequest = await this.stockDataProviderService.getDaily(symbol);
-    const alphaModelResponse = this.alphaAvantageMapperService.mapToTimeSerie(dailyRequest);
-    this.data = this.alphaAvantageMapperService.toGoogleChartModel(alphaModelResponse);//.filter(a => a[0]>new Date("2019-11-1"));
 
-  }
-
-  setSize() {
-    const mainContainer = document.querySelector('#app-main-content');
-    this.width = mainContainer.clientWidth;
-    const posibleHeight = mainContainer.clientHeight;
-    const ratioHeightWidth = 0.6;
-    if ((posibleHeight / this.width) > ratioHeightWidth ) {
-      this.height = ratioHeightWidth * this.width;
-    } else {
-      this.height = posibleHeight;
-    }
   }
 
 }
